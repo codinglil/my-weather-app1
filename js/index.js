d68fb6cef131119f2df2054f257219bc4845f835
codinglil <102619991+codinglil@users.noreply.github.com>
2022-05-19 16:17:12 -0400

    adding my existing project html, css, js and img

---
+++ js/index.js
@@ -0,0 +1,112 @@
+//search city and update name
+function searchCity(event) {
+  event.preventDefault();
+  let displayCity = document.querySelector("#search-place");
+
+  let h3 = document.querySelector("#chosen-city-name-update");
+  h3.innerHTML = `${displayCity.value.toUpperCase()}`;
+  let p = document.querySelector(".city-change-name");
+  p.innerHTML = `${displayCity.value.toUpperCase()}`;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${displayCity.value}&appid=d5ccd512023748fb33c1fa7c1f597470&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", searchCity);
+
+//get temp
+function showTemp(response) {
+  let temp = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector("#curr-temp");
+  currentTemp.innerHTML = `${temp}⁰C`;
+
+  let description = response.data.weather[0].description;
+
+  let weather = document.querySelector(".weather-description");
+  console.log(weather);
+  weather.innerHTML = `<strong>${description.toUpperCase()}</strong>`;
+}
+//adding a button listener
+function getPosition(position) {
+  navigator.geolocation.getCurrentPosition(getPosition);
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lon=${lon}&lat=${lat}&appid=d5ccd512023748fb33c1fa7c1f597470&units=metric`;
+  axios.get(apiUrl).then(showMyTemp);
+}
+let button = document.querySelector(".myCity");
+button.addEventListener("click", getPosition);
+
+function showMyTemp(response) {
+  let temp = Math.round(response.data.main.temp);
+
+  let myCurrentTemp = document.querySelector("#curr-temp");
+  myCurrentTemp.innerHTML = `${temp}⁰C`;
+  let h3 = document.querySelector("#chosen-city-name-update");
+  h3.innerHTML = response.data.name;
+  //h3.innerHTML = `It is ${temp}⁰C`;
+  let p = document.querySelector("#chosen-city-weather-description");
+  p.innerHTML = `<strong><i>${response.data.weather[0].description.toUpperCase()}</i></strong> at your location now.`;
+}
+//2 degrees
+//  function changeToC(event) {
+//  event.preventDefault();
+//  let currentTemp = document.querySelector("#curr-temp");
+//  currentTemp.innerHTML = ;
+//  }
+//  let clickC = document.querySelector(".c");
+//  clickC.addEventListener("click", changeToC);
+
+//  function changeToF(event) {
+//      event.preventDefault();
+//   let currentTemp = document.querySelector("#curr-temp");
+//  currentTemp.innerHTML = ;
+//  }
+//  let clickF = document.querySelector(".f");
+//  clickF.addEventListener("click", changeToF);
+
+//3 date manipulation
+function displayDay() {
+  let currentDay = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDay.getDay()];
+  let currentDayTitle = document.querySelector(".current-day-title");
+  currentDayTitle.innerHTML = `${day}`;
+}
+
+displayDay();
+
+function displayTodayDate() {
+  let currentDate = new Date();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[currentDate.getMonth()];
+  let day = currentDate.getDate();
+  let hour = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+
+  let currentDateTitle = document.querySelector(".current-date");
+  currentDateTitle.innerHTML = `${month} ${day}, ${hour}:${minutes}`;
+}
+displayTodayDate();
